e8781ab8e0ffd5e1820cfb7bd76ce1de4bb15c09
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2021-11-13 13:36:50 -0500

    2.2.3 Created function in IRepository
    IEnumerable<T> GetAll
    T GetFirstOrDefault

--- NikhilBookStore.DataAccess/Repository/IRepository/IRepository.cs
+++ NikhilBookStore.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NikhilBookStore.DataAccess.Repository.IRepository
 {
-    interface IRepository
+    public interface IRepository<T> where T : class
     {
+        T Get(int id);  //retrive category by id
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            String includeProperties = null  //usefull for foreign key refrences
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            String includeProperties = null
+            );
+
+
     }
 }
