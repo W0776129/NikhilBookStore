7e6a087b13e57948ec58edc0bf448c452ffeeae6
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2021-11-13 13:51:17 -0500

    2.2.5 Add Repository.cs class file
    implemented interface

---
+++ NikhilBookStore.DataAccess/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using NikhilBookStore.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NikhilBookStore.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
