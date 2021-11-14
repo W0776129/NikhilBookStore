bb02af293b8b6eb25ff0e44dbd31b059d40adcd0
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2021-11-13 20:27:43 -0500

    2.2.9  updated ICategoryRepository

--- NikhilBookStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ NikhilBookStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NikhilBookStore.Models;
 
 namespace NikhilBookStore.DataAccess.Repository.IRepository
 {
-    interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        void update(Category category);
     }
 }
