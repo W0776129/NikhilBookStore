1b7166e1aa2c69822ce1b6ec46e3b6a05ee80ab2
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2021-11-13 20:24:38 -0500

    2.2.8 Updateed CategoryRepository

--- NikhilBookStore.DataAccess/Repository/CategoryRepository.cs
+++ NikhilBookStore.DataAccess/Repository/CategoryRepository.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NikhilBookStore.DataAccess.Repository.IRepository;
+using NikhilBookStore.Models;
+using NikhilBookStore.DataAccess.Data;
 
 namespace NikhilBookStore.DataAccess.Repository
 {
-    class CategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
     }
 }
