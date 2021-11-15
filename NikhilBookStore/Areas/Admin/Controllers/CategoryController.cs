e52eb19052d679c03789acf00ea8bb5a1221045a
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2021-11-15 11:25:53 -0500

    2.3.4 Upsert modified
    and linking to it createed

--- NikhilBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ NikhilBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NikhilBookStore.DataAccess.Repository.IRepository;
+using NikhilBookStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,21 @@ namespace NikhilBookStore.Areas.Admin.Controllers
         
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                return View(category);
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
             return View();
         }
 
